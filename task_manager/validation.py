d811f3bb2159da9354b382caffd67a466588fdf2
Kelvin Korir <kibetkelvin222@gmail.com>
2026-06-02 12:43:34 +0300

    initial commit and validation functionality

---
+++ task_manager/validation.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+def validate_task_title(title):
+    if not isinstance(title, str):
+        return False, "Title must be a string"
+    
+    if len(title,strip()) < 3:
+        return False, "Title must be atleast 3 characters"
+    
+    return True, title.strip()
+
+def validate_task_description(description):
+    if not isinstance(description, str):
+        return False, "Description must be a string"
+
+    if len(description.strip()) < 5:
+        return False, "Description must be at least 5 characters"
+
+    return True, description.strip() 
+
+def validate_due_date(due_date):
+    try:
+        datetime.striptime(due_date,"%Y-%m-%d")
+        return True, due_date
+    except ValueError:
+        return False, "Date must be YYYY-MM-DD"
