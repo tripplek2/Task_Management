2352a2c546bbd076b9095b7273558829a93e7df0
Kelvin Korir <kibetkelvin222@gmail.com>
2026-06-02 16:31:34 +0300

    updated value error

--- task_manager/validation.py
+++ task_manager/validation.py
@@ -1,16 +1,12 @@
 from datetime import datetime
 
 def validate_task_title(title):
-    title = title.strip()
-
     if len(title.strip()) < 3:
         raise ValueError("Title too short")
     return title.strip()
 
 
 def validate_task_description(description):
-    description = description.strip()
-
     if len(description.strip()) < 5:
         raise ValueError("Description too short")
     return description.strip()
@@ -21,4 +17,4 @@ def validate_due_date(due_date):
         datetime.strptime(due_date, "%Y-%m-%d")
         return due_date
     except ValueError:
-        return False, "Invalid date format"
+        raise ValueError("Invalid date format")
