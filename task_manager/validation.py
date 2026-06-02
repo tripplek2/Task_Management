398e4f59292b2617eff723403fb76996d02fb8ad
Kelvin Korir <kibetkelvin222@gmail.com>
2026-06-02 16:27:47 +0300

    updated validation functionality

--- task_manager/validation.py
+++ task_manager/validation.py
@@ -3,24 +3,22 @@ from datetime import datetime
 def validate_task_title(title):
     title = title.strip()
 
-    if len(title) < 3:
+    if len(title.strip()) < 3:
         raise ValueError("Title too short")
-
-    return True, title
+    return title.strip()
 
 
 def validate_task_description(description):
     description = description.strip()
 
-    if len(description) < 5:
+    if len(description.strip()) < 5:
         raise ValueError("Description too short")
-
-    return True, description
+    return description.strip()
 
 
 def validate_due_date(due_date):
     try:
         datetime.strptime(due_date, "%Y-%m-%d")
-        return True, due_date
+        return due_date
     except ValueError:
         return False, "Invalid date format"
