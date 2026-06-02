9a60cc954a31ebb529af996d503c7a3af7d796b5
Kelvin Korir <kibetkelvin222@gmail.com>
2026-06-02 17:33:27 +0300

    update made

--- task_manager/validation.py
+++ task_manager/validation.py
@@ -1,15 +1,15 @@
 from datetime import datetime
 
 def validate_task_title(title):
-    if len(title) == 0 or len(title.strip()) == 0:
+    if len(title) == 0:
         raise ValueError("Title cannot be empty")
-    return title
+    return title.strip()
 
 
 def validate_task_description(description):
-    if len(description) == 0 or len(description.strip()) == 0:
+    if len(description) == 0:
         raise ValueError("Description cannot be empty")
-    return description
+    return description.strip()
 
 def validate_due_date(due_date):
     try:
